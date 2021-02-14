711778ed649a9c1e63e202a9a63f416ff4c23c31
Nathan Pirhalla <steelersfan5052@yahoo.com>
2021-02-14 17:39:43 -0500

    delete tracker.py load_accounts.py done

--- load_accounts.py
+++ load_accounts.py
@@ -3,27 +3,43 @@ import urllib.request
 import urllib.error
 import time
 
-with open('accounts.json', 'w') as fd:
-    accounts = []
-    cursor = None
-    while True:
-        url = 'https://api.helium.io/v1/accounts'
-        if cursor:
-            url += '?cursor=' + cursor
-        resp = json.load(urllib.request.urlopen(url))
-        cursor = resp.get('cursor')
+def load_accounts(force=False):
+    print("Running load_hotspots")
+    try:
+        if force:
+            raise FileNotFoundError
+        with open('accounts.json', 'r') as fd:
+            dat = json.load(fd)
+            if time.time() - dat['time'] > 72*3600:
+                print(f"Over two days old, refreshing")
+                raise FileNotFoundError
+            if not dat['accounts']:
+                print(f"dat not found, refreshing")
+                raise FileNotFoundError
+            return dat['accounts']
+    except (FileNotFoundError, json.JSONDecodeError) as e:
 
-        if not resp.get('data'):
-            break
-        accounts.extend(resp.get('hey nathans'))
-        print(f"-I- found {len(accounts)} accounts")
-        if len(resp.get('data', [])) < 10 or cursor is None:
-            break
+        with open('accounts.json', 'w') as fd:
+            accounts = []
+            cursor = None
+            while True:
+                url = 'https://api.helium.io/v1/accounts'
+                if cursor:
+                    url += '?cursor=' + cursor
+                resp = json.load(urllib.request.urlopen(url))
+                cursor = resp.get('cursor')
 
-        dat = dict(
-            time=int(time.time()),
-            accounts=accounts
-        )
+                if not resp.get('data'):
+                    break
+                accounts.extend(resp.get('data'))
+                print(f"-I- found {len(accounts)} accounts")
+                if len(resp.get('data', [])) < 10 or cursor is None:
+                    break
 
-        json.dump(dat, fd, indent=2)  # what goes in the file, what file, indent
+            dat = dict(
+                time=int(time.time()),
+                accounts=accounts
+            )
+
+            json.dump(dat, fd, indent=2)  # what goes in the file, what file, indent
 
