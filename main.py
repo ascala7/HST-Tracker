0f614abc739e4f66eae9830ce166566adb996115
ascala7 <45462916+ascala7@users.noreply.github.com>
2021-02-14 18:32:36 -0500

    created inital csv

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from load_accounts import load_accounts
+import csv
+import numpy as np
+import pandas as pd
+import datetime
+
 
 def main():
     print("Running Main")
@@ -6,4 +11,20 @@ def main():
     return accounts
 
 accounts = main()
-print("test")
+hst_accounts = []
+hst_balances = np.array([[""]])
+for x in accounts:
+    if x["sec_balance"] != 0:
+        hst_accounts.append(x["address"])
+        temp_bal = [x["balance"]]
+        hst_balances = np.vstack((hst_balances, temp_bal))
+
+hst_balances = np.delete(hst_balances, 0, 0)
+df = pd.DataFrame(data=hst_balances, index=hst_accounts, columns=[datetime.date])
+df.to_csv("data.csv")
+
+
+
+
+
+
