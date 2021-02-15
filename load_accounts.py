b44e782d91088e8008628b64200594379b671b55
ascala7 <45462916+ascala7@users.noreply.github.com>
2021-02-15 11:26:00 -0500

    added .gitignore and created second hst_account json code

--- load_accounts.py
+++ load_accounts.py
@@ -10,38 +10,53 @@ def load_accounts(force=False):
         if force:
             raise FileNotFoundError
         with open('accounts.json', 'r') as fd:
-            dat = json.load(fd)
-            if time.time() - dat['time'] > 72 * 3600:
-                print(f"Over two days old, refreshing")
-                raise FileNotFoundError
-            if not dat['accounts']:
-                print(f"dat not found, refreshing")
-                raise FileNotFoundError
-            return dat['accounts']
+            with open('hst_accounts.json', 'r') as hst:
+                dat = json.load(fd)
+                dat2 = json.load(hst)
+
+                if time.time() - dat['time'] > 72 * 3600:
+                    print(f"Over two days old, refreshing")
+                    raise FileNotFoundError
+                if not dat['accounts']:
+                    print(f"dat not found, refreshing")
+                    raise FileNotFoundError
+                return dat['accounts'], dat2['hst_accounts']
     except (FileNotFoundError, json.JSONDecodeError) as e:
 
         with open('accounts.json', 'w') as fd:
-            accounts = []
-            cursor = None
-            while True:
-                url = 'https://api.helium.io/v1/accounts'
-                if cursor:
-                    url += '?cursor=' + cursor
-                resp = json.load(urllib.request.urlopen(url))
-                cursor = resp.get('cursor')
+            with open('hst_accounts.json', 'w') as hst:
+                accounts = []
+                hst_accounts = []
+                cursor = None
+                while True:
+                    url = 'https://api.helium.io/v1/accounts'
+                    if cursor:
+                        url += '?cursor=' + cursor
+                    resp = json.load(urllib.request.urlopen(url))
+                    cursor = resp.get('cursor')
 
-                if not resp.get('data'):
-                    break
-                accounts.extend(resp.get('data'))
-                print(f"-I- found {len(accounts)} accounts")
-                if len(resp.get('data', [])) < 100 or cursor is None:
-                    break
+                    if not resp.get('data'):
+                        break
+                    accounts.extend(resp.get('data'))
+                    for x in resp.get('data'):
+                        if x["sec_balance"] != 0:
+                            print(x)
+                            hst_accounts.append(x)
+                    print(f"-I- found {len(accounts)} accounts")
+                    if len(resp.get('data', [])) < 100 or cursor is None:
+                        break
 
-            dat = dict(
-                time=int(time.time()),
-                accounts=accounts
-            )
+                dat = dict(
+                    time=int(time.time()),
+                    accounts=accounts
+                )
 
-            json.dump(dat, fd, indent=2)  # what goes in the file, what file, indent
+                dat2 = dict(
+                    hst_accounts=hst_accounts
+                )
 
-        return accounts
+
+                json.dump(dat, fd, indent=2)  # what goes in the file, what file, indent
+                json.dump(dat2, hst, indent=2)
+
+    return accounts, hst_accounts
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from load_accounts import load_accounts
-import csv
 import numpy as np
 import pandas as pd
 import datetime
@@ -7,21 +6,33 @@ import datetime
 
 def main():
     print("Running Main")
-    accounts = load_accounts()
-    return accounts
+    accounts, hst_accounts = load_accounts()
+    return accounts, hst_accounts
 
-accounts = main()
-hst_accounts = []
-hst_balances = np.array([[""]])
-for x in accounts:
-    if x["sec_balance"] != 0:
-        hst_accounts.append(x["address"])
-        temp_bal = [x["balance"]]
-        hst_balances = np.vstack((hst_balances, temp_bal))
+accounts, hst_accounts = main()
+print(len(accounts))
+print(len(hst_accounts))
+df = pd.read_csv("data.csv")
+data = df.to_numpy()
+hst_accounts = data[:,0]
+print(hst_accounts.shape)
 
-hst_balances = np.delete(hst_balances, 0, 0)
-df = pd.DataFrame(data=hst_balances, index=hst_accounts, columns=[datetime.date])
-df.to_csv("data.csv")
+
+
+
+
+
+# hst_accounts = []
+# hst_balances = np.array([[""]])
+# for x in accounts:
+#     if x["sec_balance"] != 0:
+#         hst_accounts.append(x["address"])
+#         temp_bal = [x["balance"]]
+#         hst_balances = np.vstack((hst_balances, temp_bal))
+#
+# hst_balances = np.delete(hst_balances, 0, 0)
+# df = pd.DataFrame(data=hst_balances, index=hst_accounts, columns=[datetime.date])
+# df.to_csv("data.csv")
 
 
 
